02a85d996133b1635a2fcaee3b30cc6de9836646
frostbtw <319670811+frostbtw2012@users.noreply.github.com>
2026-08-25 10:42:54 +0700

    adding mods
    remove immersive engineering and its addon

---
+++ kubejs/server_scripts/smith_recipe.js
@@ -0,0 +1,10 @@
+ServerEvents.recipes(event => {
+    event.shaped('tfc:metal/pickaxe/bronze',[
+        'A  ',
+        'B  ',
+    ],{
+        A: 'tfc:metal/pickaxe_head/bronze',
+        B: 'minecraft:stick',
+    }
+    )
+})
