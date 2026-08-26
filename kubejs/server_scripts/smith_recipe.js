078f74bf15ee804b597d271aba820592601a4e18
frostbtw <319670811+frostbtw2012@users.noreply.github.com>
2026-08-26 08:39:56 +0700

    remove packwiz and adding more mods :steam_happy:
    added astages and its dependancy + puff fish skill tree

--- kubejs/server_scripts/smith_recipe.js
+++ kubejs/server_scripts/smith_recipe.js
@@ -1,9 +1,12 @@
 ServerEvents.recipes(event => {
+    //Stage 0: Beginner_Smith
+
     event.shaped('tfc:metal/pickaxe/bronze',[
         'A  ',
         'B  ',
     ],{
         A: 'tfc:metal/pickaxe_head/bronze',
         B: 'minecraft:stick',
-    })
+    },)
+    .id('tfc:crafting/metal/pickaxe/bronze_specialized')
 })
---
+++ kubejs/server_scripts/smith_recipe_lock.js
@@ -0,0 +1,5 @@
+AStages.customizeStage("Beginner_Smith")
+    .playerOnly()
+    .icon('tfc:metal/hammer/copper')
+
+AStages.addRestrictionForRecipe("smith/CopperAnvil", "Beginner_Smith", "minecraft:crafting", "tfc:crafting/metal/anvil/copper")
