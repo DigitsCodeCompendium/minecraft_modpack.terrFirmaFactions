a32acc2c251a1b42d1850ec3ac062b894d5e3390
mepa <eb.hebisha@gmail.com>
2026-08-28 00:13:15 -0400

    ceremetal works and i renamed lapis but ignore that

---
+++ kubejs/server_scripts/andesite.js
@@ -0,0 +1,27 @@
+ServerEvents.recipes(event =>{
+    
+    event.shaped(
+        Item.of('terra:unfired_ceremetal'),
+
+        [
+            'AB ',    
+            'BA ',
+            '   '
+        ],{
+            A: 'tfc:powder/bismuthinite',
+            B: 'minecraft:clay_ball'
+        }
+    )
+
+    event.remove({output: 'create:andesite_alloy'})
+
+
+    event.recipes.tfc.heating(
+        'terra:unfired_ceremetal',
+        1500
+    )
+
+    .itemOutput('create:andesite_alloy')
+})
+
+
