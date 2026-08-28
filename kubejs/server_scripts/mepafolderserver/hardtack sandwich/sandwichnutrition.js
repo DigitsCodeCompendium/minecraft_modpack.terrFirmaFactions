45edb99cc8428ac949a5a5ebe05a7af81d4ace14
mepa <eb.hebisha@gmail.com>
2026-08-28 13:45:38 -0400

    hardtack sandwich + organization stuff + i removed some of the problematic mods, can be added later no startup issues :)

---
+++ kubejs/server_scripts/mepafolderserver/hardtack sandwich/sandwichnutrition.js
@@ -0,0 +1,13 @@
+TFCEvents.data(event => {
+  event.food({
+    ingredient: 'terra:hardtack_sandwich',
+    
+    food:{
+    hunger: 5,
+    edible: true,
+    decay_modifer: 1.0,
+    water: 0,
+    nutrients: [0.4, 0.0, 0.0, 0.0, 0.0]
+    }
+  })
+})
