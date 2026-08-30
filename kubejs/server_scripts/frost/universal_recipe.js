2e88b273c917299341c58302f5021c689d5fa295
frostbtw <319670811+frostbtw2012@users.noreply.github.com>
2026-08-31 03:28:20 +0700

    add supplementaries and compat

--- kubejs/server_scripts/frost/universal_recipe.js
+++ kubejs/server_scripts/frost/universal_recipe.js
@@ -1,8 +1,11 @@
 ServerEvents.recipes(event => {
-    event.shapeless(
-        Item.of('minecraft:stick'), 
-        [
-            '#tfc:twigs',
-        ])
-        .id('tfc:stick_to_twig')
+    //add
+    event.shapeless(Item.of('minecraft:stick'), ['#tfc:twigs',]).id('tfc:stick_to_twig')
+    //remove
+    event.remove({output: '#supplementaries:cannon_boats'})
+
+})
+
+RecipeViewerEvents.removeEntries('item', event => {
+	event.remove('#minecraft:boats')
 })
