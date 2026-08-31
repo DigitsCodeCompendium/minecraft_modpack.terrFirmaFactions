d93d764c5975ac1ad3a9e0764208ffa1e55bc51c
frostbtw <319670811+frostbtw2012@users.noreply.github.com>
2026-09-01 00:15:55 +0700

    looping deluxe

--- kubejs/server_scripts/frost/new_tags.js
+++ kubejs/server_scripts/frost/new_tags.js
@@ -1,29 +1,7 @@
 ServerEvents.tags('item', event => {
-    //triple_ingots
-    event.add('c:triple_ingots', 'tfc:metal/triple_ingot/copper')
-    event.add('c:triple_ingots/copper', 'tfc:metal/triple_ingot/copper')
-
-    event.add('c:triple_ingots', 'tfc:metal/triple_ingot/bronze')
-    event.add('c:triple_ingots/copper', 'tfc:metal/triple_ingot/bronze')
-
-    event.add('c:triple_ingots', 'tfc:metal/triple_ingot/bismuth_bronze')
-    event.add('c:triple_ingots/copper', 'tfc:metal/triple_ingot/bismuth_bronze')
-
-    event.add('c:triple_ingots', 'tfc:metal/triple_ingot/black_bronze')
-    event.add('c:triple_ingots/copper', 'tfc:metal/triple_ingot/black_bronze')
-
-    event.add('c:triple_ingots', 'tfc:metal/triple_ingot/wrought_iron')
-    event.add('c:triple_ingots/copper', 'tfc:metal/triple_ingot/wrought_iron')
-
-    event.add('c:triple_ingots', 'tfc:metal/triple_ingot/steel')
-    event.add('c:triple_ingots/copper', 'tfc:metal/triple_ingot/steel')
-
-    event.add('c:triple_ingots', 'tfc:metal/triple_ingot/black_steel')
-    event.add('c:triple_ingots/copper', 'tfc:metal/triple_ingot/black_steel')
-
-    event.add('c:triple_ingots', 'tfc:metal/triple_ingot/blue_steel')
-    event.add('c:triple_ingots/copper', 'tfc:metal/triple_ingot/blue_steel')
-
-    event.add('c:triple_ingots', 'tfc:metal/triple_ingot/red_steel')
-    event.add('c:triple_ingots/copper', 'tfc:metal/triple_ingot/red_steel')
-})
+    const metals = ['copper', 'bronze','bismuth_bronze','black_bronze','wrought_iron','steel','black_steel','red_steel','blue_steel']
+    metals.forEach(metal => {
+        event.add('c:triple_ingots', `tfc:metal/triple_ingot/${metal}`)
+        event.add(`c:triple_ingots/${metal}`, `tfc:metal/triple_ingot/${metal}`)
+        });
+    })
